4df318de8ab8f63a171e32b86b49bdee5be3a1b6
lior-yossef <lior.yossef@post.runi.ac.il>
2024-12-05 22:36:49 +0200

    Update output

--- Scrabble.java
+++ Scrabble.java
@@ -119,7 +119,7 @@ public class Scrabble {
 					System.out.println(input + " earned " + wordScore + " points. Score: " + score + " points");
 				}
 			} else {
-				System.out.println("No such word in the dictionary. Try again.");
+				System.out.println("Invalid word. Try again.");
 			}
 		}
 		if (hand.length() == 0) {
